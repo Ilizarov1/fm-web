fcd231d512fb3393678390d1f2d9fe656cf486a2
Ilizarov1 <1224722105@qq.com>
2021-05-06 23:55:42 +0800

    开始做后端

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    players: [],
+    status: 200
   },
   mutations: {
+    // 更新players
+    updatePlayers(state, newPlayers) {
+      state.players = newPlayers
+    },
+    // 更新status
+    updateStatus(state, newStatus) {
+      state.status = newStatus
+    }
   },
   actions: {
+    async getPlayers({ commit }) {
+      commit('updateStatus', 200)
+      const { status, data } = await axios.get('player/getAll')
+      if (status !== 200) {
+        commit('updateStatus', status)
+      } else {
+        commit('updatePlayers', data)
+      }
+    }
   },
-  modules: {
-  }
+
+  modules: {}
 })
--- src/views/Players/List.vue
+++ src/views/Players/List.vue
@@ -1,10 +1,21 @@
 <template>
   <el-row :gutter="8">
     <el-row style="margin-bottom: 10px">
-      <el-button type="primary" size="mini">加入一线队</el-button>
-      <el-button type="danger" size="mini">移出一线队</el-button>
-      <el-button type="success" size="mini">休假</el-button>
-      <el-button type="danger" size="mini">召回</el-button>
+      <el-button-group>
+        <el-button type="primary" size="mini" @click="fstTeam(1)"
+          >加入一线队</el-button
+        >
+        <el-button type="danger" size="mini" @click="fstTeam(0)"
+          >移出一线队</el-button
+        >
+        <el-button type="success" size="mini" @click="vacation(0)"
+          >休假</el-button
+        >
+        <el-button type="danger" size="mini" @click="vacation(1)"
+          >召回</el-button
+        >
+      </el-button-group>
+
       <el-input
         placeholder="请输入内容"
         v-model="search"
@@ -25,124 +36,145 @@
       </el-input>
     </el-row>
     <el-row type="flex" :gutter="8">
-      <el-table :data="playerList" ref="playerTable" style="width: 1060px">
+      <el-table
+        :data="players"
+        ref="playerTable"
+        style="width: 1060px"
+        max-height="550"
+        @selection-change="selectionHandler"
+      >
         <el-table-column type="selection"></el-table-column>
-        <el-table-column prop="isFstTeam" label="一线队" width="80">
+        <el-table-column label="一线队" width="80">
+          <template slot-scope="scope">
+            {{ getIsFstTeam(scope.row.isFstTeam) }}
+          </template>
         </el-table-column>
         <el-table-column prop="name" label="姓名" width="100"></el-table-column>
-        <el-table-column prop="position" label="位置"></el-table-column>
+        <el-table-column prop="skilledRole" label="位置"></el-table-column>
         <el-table-column
           prop="rate"
           label="综合评分"
           width="100"
         ></el-table-column>
-        <el-table-column
-          prop="isWork"
-          label="工作/休假/伤停"
-          width="120"
-        ></el-table-column>
-        <el-table-column
-          prop="isHealthy"
-          label="健康状态"
-          width="100"
-        ></el-table-column>
+        <el-table-column label="工作/休假/伤停" width="120">
+          <template slot-scope="scope">
+            {{ getIsWork(scope.row.isWork) }}
+          </template>
+        </el-table-column>
+        <el-table-column label="健康状态" width="100">
+          <template slot-scope="scope">
+            {{ getIsHealthy(scope.row.isHealthy) }}
+          </template>
+        </el-table-column>
       </el-table>
     </el-row>
-    <el-row type="flex" justify="end">
-      <div class="pager-box">
-        <el-pagination
-          layout="prev, pager, next"
-          :total="50"
-          class="pagination"
-        >
-        </el-pagination>
-      </div>
-    </el-row>
+    <!--    <el-row type="flex" justify="end">-->
+    <!--      <div class="pager-box">-->
+    <!--        <el-pagination-->
+    <!--          layout="prev, pager, next"-->
+    <!--          :total="50"-->
+    <!--          class="pagination"-->
+    <!--        >-->
+    <!--        </el-pagination>-->
+    <!--      </div>-->
+    <!--    </el-row>-->
   </el-row>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex'
+import qs from 'qs'
 export default {
   name: 'List',
   data() {
     return {
-      playerList: [
-        {
-          name: 'L.Messi',
-          position: 'RW/ST/CF',
-          rate: 93,
-          potential: 93,
-          isWork: '工作中',
-          isHealthy: '状态良好',
-          isFstTeam: '首发'
-        },
-        {
-          name: 'L.Messi',
-          position: 'RW/ST/CF',
-          rate: 93,
-          potential: 93,
-          isWork: '工作中',
-          isHealthy: '状态良好',
-          isFstTeam: '首发'
-        },
-        {
-          name: 'L.Messi',
-          position: 'RW/ST/CF',
-          rate: 93,
-          potential: 93,
-          isWork: '工作中',
-          isHealthy: '状态良好',
-          isFstTeam: '首发'
-        },
-        {
-          name: 'L.Messi',
-          position: 'RW/ST/CF',
-          rate: 93,
-          potential: 93,
-          isWork: '工作中',
-          isHealthy: '状态良好',
-          isFstTeam: '首发'
-        },
-        {
-          name: 'L.Messi',
-          position: 'RW/ST/CF',
-          rate: 93,
-          potential: 93,
-          isWork: '工作中',
-          isHealthy: '状态良好',
-          isFstTeam: '首发'
-        },
-        {
-          name: 'L.Messi',
-          position: 'RW/ST/CF',
-          rate: 93,
-          potential: 93,
-          isWork: '工作中',
-          isHealthy: '状态良好',
-          isFstTeam: '首发'
-        },
-        {
-          name: 'L.Messi',
-          position: 'RW/ST/CF',
-          rate: 93,
-          potential: 93,
-          isWork: '工作中',
-          isHealthy: '状态良好',
-          isFstTeam: '首发'
-        },
-        {
-          name: 'L.Messi',
-          position: 'RW/ST/CF',
-          rate: 93,
-          potential: 93,
-          isWork: '工作中',
-          isHealthy: '状态良好',
-          isFstTeam: '首发'
-        }
-      ],
       search: '',
-      searchType: 'name'
+      searchType: 'name',
+      selected: []
     }
+  },
+  computed: {
+    ...mapState(['players'])
+  },
+  methods: {
+    ...mapActions(['getPlayers']),
+    // 表格
+    getIsWork(isWork) {
+      switch (isWork) {
+        case 0:
+          return '休假中'
+        case 1:
+          return '工作中'
+        case 2:
+          return '伤病中'
+      }
+    },
+    getIsHealthy(isHealthy) {
+      switch (isHealthy) {
+        case 1:
+          return '状态良好'
+        case 0:
+          return '存在伤病'
+      }
+    },
+    getIsFstTeam(isFstTeam) {
+      switch (isFstTeam) {
+        case 0:
+          return '待定'
+        case 1:
+          return '一线队'
+      }
+    },
+    // 处理表格选择
+    selectionHandler(e) {
+      this.selected = e
+    },
+    // 加入一线队
+    async fstTeam(mod) {
+      if (this.selected.length === 0) {
+        return this.$message.error('请选择')
+      }
+      for (const player of this.selected) {
+        await this.$http.put(
+          'player/fstTeam',
+          qs.stringify({
+            id: player.id,
+            mod
+          }),
+          {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            }
+          }
+        )
+      }
+      await this.getPlayers()
+    },
+    // 休假
+    async vacation(mod) {
+      if (this.selected.length === 0) {
+        return this.$message.error('请选择')
+      }
+      for (const player of this.selected) {
+        await this.$http.put(
+          'player/vacation',
+          qs.stringify({
+            id: player.id,
+            mod
+          }),
+          {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            }
+          }
+        )
+      }
+      await this.getPlayers()
+    }
+  },
+  mounted() {
+    this.getPlayers()
+    this.players = this.$store.state.players
   }
 }
 </script>
